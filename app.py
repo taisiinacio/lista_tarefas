8bf167688bbb749a6129b91050cca573823f08ff
taisiinacio <taisi.inacio@gmail.com>
2024-03-11 15:52:58 -0300

    Commit inicial do projeto

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from pymongo import MongoClient
+
+#instancia a app Flask
+app = Flask('_name_')
+
+#cliente de conecção com o mongo
+client = MongoClient('localhost', 27017)
+
+#concecta no db lista_tarefas
+db = client.lista_tarefas
+
+#acessa a collection
+tarefas = db.tarefas
+
+todas_tarefas = tarefas.find()
+for t1 in todas_tarefas:
+    print(t1)
